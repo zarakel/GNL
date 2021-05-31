2c2897f470f26f920de0e564412ee8ecef528bde
Jerome Buan <jbuan@c3r2p2.42nice.fr>
2021-05-31 18:39:11 +0200

    leseuletlunique

---
+++ get_next_line_utils.c
@@ -0,0 +1,107 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbuan <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/20 15:58:11 by jbuan             #+#    #+#             */
+/*   Updated: 2021/05/20 17:28:16 by jbuan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	src_len;
+	size_t	i;
+
+	src_len = 0;
+	while (src[src_len] != '\0')
+	{
+		src_len++;
+	}
+	if (dstsize == 0)
+		return (src_len);
+	i = 0;
+	while (src[i] != '\0' && i < (dstsize - 1))
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (src_len);
+}
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	src_len;
+	size_t	dst_len;
+	size_t	i;
+
+	src_len = ft_strlen(src);
+	dst_len = ft_strlen(dst);
+	i = 0;
+	while (src[i] && dst_len + i + 1 < dstsize)
+	{
+		dst[dst_len + i] = src[i];
+		i++;
+	}
+	dst[dst_len + i] = '\0';
+	if (dstsize < dst_len)
+		return (dstsize + src_len);
+	return (dst_len + src_len);
+}
+
+char	*ft_strdup(char *s1)
+{
+	char	*dst;
+	int		i;
+
+	i = ft_strlen(s1);
+	dst = (char *)malloc(sizeof(*dst) * (i + 1));
+	if (!(dst))
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		dst[i] = s1[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*c;
+	size_t	lena;
+	size_t	lenb;
+
+	if (!s1 && !s2)
+		return (NULL);
+	if (!s1)
+		return (ft_strdup(s2));
+	if (!s2)
+		return (ft_strdup(s1));
+	lena = ft_strlen(s1);
+	lenb = ft_strlen(s2);
+	c = (char *)malloc(sizeof(char) * (lena + (lenb + 1)));
+	if (!c)
+		return (NULL);
+	ft_strlcpy(c, s1, lena + 1);
+	free(s1);
+	ft_strlcat(c + (lena), s2, lenb + 1);
+	return (c);
+}
